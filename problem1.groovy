bf24d68b0c82354617287ddda449db99c042cbd3
onahump <oswaldo@makingdevs.com>
2017-12-03 15:52:42 -0600

    Corrigiendo y comentando el problema n°1

--- problem1.groovy
+++ problem1.groovy
@@ -1,12 +1,23 @@
-def lista = 1..10
+def lista = 1..9
 def lista2 = []
 
-size = lista.size()
+size = lista.size()   //Tamaño de la lista1
 
-for(i=lista[1]; lista=size; i++){
-  if (i % 3 == 0 || i % 5 == 0){
-    lista2.add(i)
-  }
+for(i=0; i<size; i++){    //Itera la lista
+    if(lista[i] % 3 == 0 || lista[i] % 5 == 0){  //Si un numero de la lista en la posicion i es divisible entre 3 y 5
+          lista2.add(lista[i])                   // Agrega el numero a la lista 2
+            }
 }
 
-println lista2
+println "Los numero que integran la nueva lista son " + lista2
+
+size2 = lista2.size()  //Tamaño de la lista2
+
+def sum = 0       //Definimos la variable suma igual a 0
+
+for(j=0;j<size2; j++){ //Iteramos la lista 2
+    sum += lista2[j]   //La suma va a ser igual a la suma mas el numero que este en la posiicon j de la lista (sum = sum + lista[j])
+}
+
+println "La suma de todos los numeros dentro de la lista es " + sum 
+
