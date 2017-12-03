f6c404ca072997855c7f1c96a5cecf173568ec47
onahump <oswaldo@makingdevs.com>
2017-12-03 17:35:47 -0600

    Aumentando tamaño de la lista a 999

--- problem1.groovy
+++ problem1.groovy
@@ -1,4 +1,4 @@
-def lista = 1..9
+def lista = 1..999
 def lista2 = []
 
 size = lista.size()   //Tamaño de la lista1
@@ -19,5 +19,5 @@ for(j=0;j<size2; j++){ //Iteramos la lista 2
     sum += lista2[j]   //La suma va a ser igual a la suma mas el numero que este en la posiicon j de la lista (sum = sum + lista[j])
 }
 
-println "La suma de todos los numeros dentro de la lista es " + sum 
+println "La suma de todos los numeros dentro de la lista es " + sum
 
