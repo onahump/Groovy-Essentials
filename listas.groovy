67879939f9b21ae739666b8b86f7a5a501147071
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2017-12-04 17:33:46 -0600

    Creando  problema de lista con metodo

---
+++ listas.groovy
@@ -0,0 +1,10 @@
+def lista1 = [1,2,3,6,6]
+def lista2 = ["a", "b", "c", "d"]
+
+def algoConListas(ArrayList algunaLista){
+   algunaLista.each{ 
+       println it
+   }
+}
+
+algoConListas(lista1 )
