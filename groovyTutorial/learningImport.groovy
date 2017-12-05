069f63fd5e3387c77e10deed41b21a5491d1c7fb
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2017-12-05 15:32:43 -0600

    Aprendiendo imports

---
+++ groovyTutorial/learningImport.groovy
@@ -0,0 +1,22 @@
+import java.time.Year // import the Year class from the java.time package
+import java.time.* // * import all classes in the java.time package
+//static imports
+import static java.lang.Math.PI //importing the PI constant
+import static java.lang.Math.PI as ultrapi //static import with aliasing
+import static java.util.UUID.randomUUID as generateID// importing a random id
+import static java.lang.Math.* //Import all static elements in Math and let you refer to them directly in the program
+
+//import java.time.Year
+println Year.now()
+println PI
+
+
+//Static imports
+println ultrapi
+println generateID()
+
+
+
+
+
+
