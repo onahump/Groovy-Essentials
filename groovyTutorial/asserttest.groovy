b3543276ee4ff1d9609a8adb448733108238130e
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2017-12-05 14:42:40 -0600

    Agregando ejemplos de asserts

--- groovyTutorial/asserttest.groovy
+++ groovyTutorial/asserttest.groovy
@@ -1,4 +1,6 @@
 def i = 10
 def j = 10
 
-assert i == j : Math.sqrt(64)
+assert i == j : Math.sqrt(64) // Agregando una expresion de groovy como error
+assert 1 == 2
+assert true == false : 'true cannot be false' //Expression agregada cini error"
