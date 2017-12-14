fb7b4637a2e5de028372d008176a5726ee969f3c
onahump <oswaldo@makingdevs.com>
2017-12-13 22:25:48 -0600

    Aprendiendo variables en groovy

---
+++ variables.groovy
@@ -0,0 +1,15 @@
+def score
+score = 10
+println score
+score = 40
+println score
+
+def myNumber, myName //Variables oneline
+def number, number2 //Variables oneline
+(number,number2) = [10,20] //values assigned from a list
+
+assert number == 10
+assert number2 == 20
+
+
+
