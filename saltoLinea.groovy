1ca095891850046b1898fb085fa951c322fda9a1
onahump <oswaldo@makingdevs.com>
2017-12-22 00:07:02 -0600

    Agregando suma de cada elemento a una lista

--- saltoLinea.groovy
+++ saltoLinea.groovy
@@ -1,6 +1,8 @@
+lista = []
+
 new File('file.txt').eachLine { line ->
    def linea = line.split('x')
    def convert = linea.collect{it.toInteger()}
-   println convert.sum()
-
+   lista.add(convert.sum())
 }
+println lista
