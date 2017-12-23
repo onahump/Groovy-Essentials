997b5da868c13ece33774004bd8b41667b9132ef
onahump <oswaldo@makingdevs.com>
2017-12-23 12:12:39 -0600

    Corrigiendo mis champurradas :p

--- adventOfCode2_1.groovy
+++ adventOfCode2_1.groovy
@@ -13,7 +13,7 @@ def getDimensionsList(def textFile){
     boxesDimensions.each{ boxDimensions ->
       def p1 = boxDimensions[0]*2 + boxDimensions[1]*2
       def p2 = boxDimensions[1]*2 + boxDimensions[2]*2
-      def p3 = boxDimensions[1]*2 + boxDimensions[0]*2
+      def p3 = boxDimensions[2]*2 + boxDimensions[0]*2
 
       def smallestPerimeter = Math.min(p1,Math.min(p2,p3))
       def bowOfRibbon = boxDimensions[0] * boxDimensions[1] * boxDimensions[2]
