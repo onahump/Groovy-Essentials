f9a573bdaa9483bed7b8bd643b0f9abe55142fcd
onahump <oswaldo@makingdevs.com>
2017-12-27 21:35:34 -0600

    Agregando problema dia 3 groovy

---
+++ day3.groovy
@@ -0,0 +1,17 @@
+archivoTexto = new File('day3.txt')
+
+santaPosition = [x:0,y:0]
+casasVisitadas = 1
+
+println santaPosition
+println santaPosition.x.getClass()
+
+
+
+def readFile(def file){
+    file.each{ i ->
+      println i
+    }
+}
+
+def coordenadas = readFile(archivoTexto)
