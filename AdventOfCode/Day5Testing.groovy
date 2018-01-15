f58a1df2f082ab99d3957d07bee5670454cc9b51
onahump <oswaldo@makingdevs.com>
2018-01-14 23:08:55 -0600

    Pasando las primeras dos pruebas

--- AdventOfCode/Day5Testing.groovy
+++ AdventOfCode/Day5Testing.groovy
@@ -4,16 +4,55 @@ class Day5Testing extends GroovyTestCase{
 
 
 	void testOne(){
-		assert helper.isNaughty("Hola Mundo") == 3
+		assert helper.isNaughty("ugknbfddgicrmopn") == "naughty"
 	} 
+
+	void testTwo(){
+		assert helper.isNaughty2("abcdefghii") == "naughty"
+	}
+	
+	void testThree(){
+		assert helper.isNaughty3("oipciqolm") == "naughty"
+	}
+
 }
 
 class Helper{
 
 	def isNaughty(String linea){
-		def regex = ~/[aeiouAEIOU]/ || ~/([a-z])\1/ ||
-		linea.findAll(regex).size()
+		def regex = ~/[aeiouAEIOU]/
+		 // ~/([a-z])\1/ 
+		if (linea.findAll(regex)){
+			linea  = "naughty"
+		}
+		else{
+			linea = "nice"
+		}
+	linea
+	}
+
+	def isNaughty2(String linea){
+		def regex = ~/([a-z])\1/ 
+		if (linea.findAll(regex)){
+			linea  = "naughty"
+		}
+		else{
+			linea = "nice"
+		}
+	linea
 	}
 
 
+	def isNaughty3(String linea){
+		def regex = ~/^a*b*c*d*e*f*g*h*i*j*k*l*m*n*o*p*q*r*s*t*u*v*w*x*y*z*+/ 
+		if (linea.findAll(regex)){
+		    linea  = "naughty"
+		}
+		else{
+		    linea = "nice"
+		}
+		print linea
+	}
 }
+
+
