a12c44cc74039c668d476e2fbef7598191de2a91
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-01-16 14:09:14 -0600

    Testeando el dia 5

--- AdventOfCode/Day5Testing.groovy
+++ AdventOfCode/Day5Testing.groovy
@@ -2,51 +2,91 @@ class Day5Testing extends GroovyTestCase{
 
   def helper = new Helper()
 
-
 	void testOne(){
-		assert helper.isNaughty("ugikpan") == true
+		assert helper.haveMoreThan3Vowels("tetipaa") == "nice"
 	} 
 
 	void testTwo(){
-		assert helper.isNaughty2("abcdefghhhhi") == true
+		assert helper.notHaveAaBbCcDd("jchzalrndimnmhpaa") == "nice"
 	}
 	
 	void testThree(){
-		assert helper.isNaughty3("oipciqolxm") == true
+		assert helper.notHaveAbBcPqXY("jchzpbcrnumiamhp") == "naughty"
 	}
 
+	void testFour(){
+		assert helper.isNice("aeiddcd") == "nice"
+	}
 }
 
 class Helper{
-
-	def isNaughty(String linea){
-		def regex = ~/[aeiou].*[aeiou].*[aeiou]/
-		if (linea.findAll(regex)){
-			true
+	def haveMoreThan3Vowels(String linea){
+		def vocales = 0
+		linea.size().times{
+			if(linea[it]== 'a'| linea[it] == 'e'| linea[it] == 'i'| linea[it] == 'o'| linea[it] == 'u'){
+				vocales ++
+			}
+		}  
+		if(vocales >= 3){
+			"nice"
 		}
 		else{
-			false
+			"naughty"
 		}
 	}
 
-	def isNaughty2(String linea){
-		def regex = ~/([a-z])\1/ 
+	def notHaveAaBbCcDd(String linea){
+		def regex = ~/aa|bb|cc|dd/ 
 		if (linea.findAll(regex)){
-			true
+			"nice"
 		}
 		else{
-			false
+			"naughty"
 		}
 	}
 
-
-	def isNaughty3(String linea){
+	def notHaveAbBcPqXY(String linea){
 		def regex = ~/ab|bc|pq|xy/
 		if (linea.findAll(regex)){
-			true
+			"naughty"
 		}
 		else{
-			false
+			"nice"
 		}
+	}
+
+	def isNice(String linea){
+		Integer vocales = 0
+		Integer errores = 0
+		Integer doble = 0
+
+			linea.size().times{
+				if(linea[it] == 'a'|| linea[it] == 'e'|| linea[it] == 'i'|| linea[it] == 'o'|| linea[it] == 'u'){
+					vocales ++
+				}
+				/*if( it != linea.size()-1 ){
+		        	if( linea[it] == linea[it+1]){
+		            	doble++
+		        	}
+		    //ver que el par no sea ab cd pq xy
+		      		if( (linea[it]+linea[it+1]) == 'ab'|| (linea[it]+linea[it+1]) == 'cd'||(linea[it]+linea[it+1]) == 'pq'||(linea[it]+linea[it+1]) == 'xy'){
+		            	errores++
+		        	}
+		    	}
+			}*/	
+			// aa, bb, cc, or dd
+		    if(linea.findAll(~/([a-z])\1/){
+		         doble++
+		    }
+			//ver que el par no sea ab, cd, pq, or xy
+		    if(linea.findAll(~/ab|cd|pq|xy/)){
+		        errores++
+		    }	
+			if (vocales >= 3 && doble >0 && errores == 0){
+				"nice"
+			}
+			else{
+				"naughty"
+			}	
 	}
 }
