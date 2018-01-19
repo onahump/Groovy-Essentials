0580f50be37178ecc64dd49e7517c6fd2302fc55
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-01-19 17:10:19 -0600

    creando ejemplo de groovlets

---
+++ Ejemplos/Groovlet/JjEjemplo/index.groovy
@@ -0,0 +1,11 @@
+html.html{
+  head {
+    title 'Facturacion MakingDevs'
+  }
+  body {
+    h1 'Bienvenido al sistema de Facturacion de MakingDevs'
+    h4 "$context"
+    h4 "$application"
+  }
+
+}
